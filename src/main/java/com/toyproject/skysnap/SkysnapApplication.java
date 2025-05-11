931e80ef64fcd2c22036c66c8562fa0ea394da24
soyonic <soyonic00@gmail.com>
2025-05-11 15:50:17 +0900

    project init.

---
+++ src/main/java/com/toyproject/skysnap/SkysnapApplication.java
@@ -0,0 +1,13 @@
+package com.toyproject.skysnap;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SkysnapApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SkysnapApplication.class, args);
+	}
+
+}
