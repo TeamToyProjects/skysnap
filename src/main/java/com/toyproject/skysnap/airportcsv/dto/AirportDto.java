7e9aed35f66bb3d50e98536f0e98e1c288e5eb09
MinEquals0 <minequals0@gmail.com>
2025-05-13 17:25:40 +0900

    feat: AirportDto 완성

--- src/main/java/com/toyproject/skysnap/airportcsv/dto/AirportDto.java
+++ src/main/java/com/toyproject/skysnap/airportcsv/dto/AirportDto.java
@@ -1,15 +1,15 @@
 package com.toyproject.skysnap.airportcsv.dto;
 
-// 사용자 응답용
-
 import lombok.Builder;
 import lombok.Data;
 
+
+// 사용자 응답용
 @Data
 @Builder
 public class AirportDto {
 
-    private String name;
+    private String name;  // 공항 이름
     private String country;
     private String iata;
     private String icao;
