8827467b87bbf2abb49bfb73d723a780af5283ed
MinEquals0 <minequals0@gmail.com>
2025-05-13 17:24:50 +0900

    feat: AirportCsvDto 완성

--- src/main/java/com/toyproject/skysnap/airportcsv/dto/AirportCsvDto.java
+++ src/main/java/com/toyproject/skysnap/airportcsv/dto/AirportCsvDto.java
@@ -5,6 +5,7 @@ import lombok.Data;
 // CSV 파일 파싱용
 @Data
 public class AirportCsvDto {
+
     private String name;  // 공항 이름
     private String municipality;  // 공항 위치한 도시명
     private String isoCountry;  // 국가코드
