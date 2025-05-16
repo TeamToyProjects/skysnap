938c9922869a0b8244ffa1c7cd49d74263042ffa
MinEquals0 <minequals0@gmail.com>
2025-05-16 15:55:36 +0900

    feat: CountryDto 완성

---
+++ src/main/java/com/toyproject/skysnap/airportdat/dto/CountryDto.java
@@ -0,0 +1,15 @@
+package com.toyproject.skysnap.airportdat.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class CountryDto {
+    private String code;
+    private String name;
+}
