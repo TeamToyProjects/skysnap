c8c2b499019a2fe5b6e85e1be3016abe034dc4d9
MinEquals0 <minequals0@gmail.com>
2025-05-22 19:55:34 +0900

    rename : 클래스명 변경

--- src/main/java/com/toyproject/skysnap/airportdat/model/AirportInfoParser.java
+++ src/main/java/com/toyproject/skysnap/airportdat/model/AirportInfoParser.java
@@ -6,7 +6,7 @@ import lombok.Getter;
 // airports.dat 각 행을 표현, dat 파일을 읽어서 메모리에 올리는 용도
 @Getter
 @AllArgsConstructor
-public class AirportInfo {
+public class AirportInfoParser {
     private String name;
     private String city;
     private String country;
