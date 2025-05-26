1b14f42113e06f8d14387ad2242c4f0532ea893b
MinEquals0 <minequals0@gmail.com>
2025-05-26 18:57:50 +0900

    feat: FlightSearchRequest 완성

---
+++ src/main/java/com/toyproject/skysnap/filghtsearch/dto/FlightSearchRequest.java
@@ -0,0 +1,22 @@
+package com.toyproject.skysnap.filghtsearch.dto;
+
+
+import java.time.LocalDate;
+
+// 사용자 검색 조건을 담는 요청 DTO
+public class FlightSearchRequest {
+    private String departureCity;  // 출발도시
+    private String arriveCity;     // 도착도시
+
+    private LocalDate departureDate;  // 출발일
+    private LocalDate returnDate;    // 도착일, 선택 옵션, null 가능
+
+    private boolean baggageRequired;
+    private boolean mealRequired;
+
+    private Boolean direct;  // T면 직항, F면 경유, null이면 모두 포함
+
+
+
+
+}
