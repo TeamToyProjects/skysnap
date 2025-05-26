ee86d532e492f5b7935e0002d596fa3870e7b09c
MinEquals0 <minequals0@gmail.com>
2025-05-26 19:43:23 +0900

    rename : 클래스 이름 변경, 필드명 추가

--- src/main/java/com/toyproject/skysnap/filghtsearch/dto/SeatGrade.java
+++ src/main/java/com/toyproject/skysnap/filghtsearch/dto/SeatGrade.java
@@ -1,8 +1,10 @@
 package com.toyproject.skysnap.filghtsearch.dto;
 
 // 스카이스캐너에서는 항공편 선택했을 때 각 좌석등급에 대한 요금이 뜸
-//public enum SeatType {
-//    ECONOMY,
-//    BUSINESS,
-//    FIRST
-//}
+// 사용 안할 수도 있음
+public enum SeatGrade {
+    ECONOMY,
+    PREMIUM_ECONOMY,
+    BUSINESS,
+    FIRST
+}
